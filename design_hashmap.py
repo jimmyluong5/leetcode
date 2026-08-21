f1e90a056411deb96cc6b1efb59e1f0bfb752907
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-22 00:01:40 +0800

    merge two sorted arrays leetcode

--- design_hashmap.py
+++ design_hashmap.py
@@ -11,7 +11,31 @@ void remove(key) removes the key and its corresponding value if the map contains
 [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
 
 Output: [null, null, null, 1, -1, null, 1, null, -1] """
+
+
+""" Input
+["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+Output
+[null, null, null, 1, -1, null, 1, null, -1]
+
+Explanation
+MyHashMap myHashMap = new MyHashMap();
+myHashMap.put(1, 1); // The map is now [[1,1]]
+myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+  """
+
+
+
 #make node
+
+input = ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
 
 class node():
     def __init__(self, key = -1, val = -1, next=None):
@@ -83,4 +107,4 @@ sol = MyHashMap()
 sol.put(1,1)
 sol.put(2,2)
 print(sol.get(1))
-print(sol.get(3))   
+print(sol.get(3)) 
