6385dcde5baf3fceaee6e26e18821eaf1e85c3d9
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-23 12:07:48 +0800

    trees saving

--- tree.py
+++ tree.py
@@ -1,12 +1,11 @@
-import roman_to_integer
 class TreeNode():
     def __init__(self, val, left = None, right = None):
         self.left = left
         self.right = right
         self.val = val
 
-def __str__(self):
-    return str(self.val)
+    def __str__(self):
+        return str(self.val)
 
 #create the nodes
 A = TreeNode(1)
@@ -26,3 +25,41 @@ C.right = G
 
 print(A)
 
+
+#pre-order traversal DFS
+
+def preorder(root):
+    #we need a base case so if the root is None then return
+    if root == None:
+        return 
+    else: #we process the node first then go left then right
+        print(root)
+        preorder(root.left)
+        preorder(root.right)
+
+preorder(A)
+# 1 2 4 5 3 10 11
+print('\n')
+#in order traversal dfs
+def inorder(root):
+    if root == None:
+        return
+    else: #we go left first then root then right
+        inorder(root.left)
+        print(root)
+        inorder(root.right)
+inorder(A) # left then root then right
+# 4 2 5 1 10 3 11
+print('\n')
+
+#post order traversal 
+
+def postorder(root):
+    if root == None:
+        return
+    else:
+        postorder(root.left)
+        postorder(root.right)
+        print(root)
+postorder(A)
+#4 2 5 10 11 3 1
