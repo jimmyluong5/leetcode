74892edb76a9c5d2fb78c58a41e94142c42302bf
jimmyluong5 <jimmyluong@yahoo.com>
2026-09-13 11:45:33 +0800

    redid majority element

--- arrays_hashmaps/majority_element.py
+++ arrays_hashmaps/majority_element.py
@@ -2,6 +2,9 @@
 
 The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
+for hashmap problems always increase the frequency first because 
+its actually the 2nd iteration of going in the loop if the number
+exists already in the hashmap
  
 
 Example 1:
@@ -13,25 +16,31 @@ Example 2:
 Input: nums = [2,2,1,1,1,2,2]
 Output: 2 """
 
-nums = [2, 2, 1, 1, 1, 1, 2, 3]
-n = len(nums)
+nums = [2, 2, 1, 1, 1, 2, 2]
+#if the last element was a 9, the algorithm wouldn't work.
+
 class Solution:
     def majorityElement(self, nums):
         #create hashmap
         map = {}
-
-        #loop through the nums array
+        n = len(nums)
+        #for hashmaps you should always increase the freq first
+        #because if the number already exists in the hashmap then 
+        #the loop is actually the 2nd iteration
         for num in nums:
             if num in map:
-                if map[num] == n//2:
+                #increase the frequency
+                map[num]+=1
+
+                #check for condition
+                if map[num] > n//2:
                     return num
-                else:
-                    #increase the frequency
-                    map[num] +=1
             else:
-                #add the number to the hashmap
+                #add it to the hashmap
                 map[num] = 1
         return num
+       
+       
 sol = Solution()
 
 print(sol.majorityElement(nums))
