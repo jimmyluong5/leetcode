0ef4e2dfc21d42d9f0ce4208166f82eb9b745761
jimmyluong5 <jimmyluong@yahoo.com>
2026-09-19 21:41:51 +0800

    finished rotate arrays

--- arrays_hashmaps/rotate_array.py
+++ arrays_hashmaps/rotate_array.py
@@ -33,21 +33,31 @@ Constraints:
 
 #this question is actually easy intuitively
 #
-
-class Solution():
-    def rotate(self, nums):
-        #reverse it once
-        #then reverse the first k elements from 0 to k-1
-        #then reverse the n-k elements
+nums = [1, 2, 3, 4, 5, 6, 7]
+nums1 = [1, 2, 3, 4, 5, 6, 7]
+class Solution(object):
+    def rotate(self, nums, k):
         n = len(nums)
-        k%=n
+        k = k%n
 
         def helper(arr, left, right):
             while left<right:
-                arr[left], arr[right] = arr[right], arr[left]
+                #swap
+                arr[left], arr[right]=arr[right], arr[left]
                 left+=1
                 right-=1
             return arr
-        #we reverse once
+        
+        #we need to swap the array once, then for the first k elements reverse that then reverse from k to n-1
+        arr = helper(nums, 0, n-1)
+        arr = helper(arr, 0, k-1)
+        arr = helper(arr, k, n-1)
+        return arr
+sol = Solution()
+print(sol.rotate(nums, 1))
+#should be [7, 1, 2, 3, 4, 5, 6]
 
-
+print("\n")
+print(sol.rotate(nums1, 3)) #if you use the same array, the previous old values will be there
+#arrays are mutable in python.
+#should be [5,6,7,1,2,3,4]
