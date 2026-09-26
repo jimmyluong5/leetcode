c0ff14c4f33daa025600092aa63803e21bdf1784
jimmyluong5 <jimmyluong@yahoo.com>
2026-09-26 22:30:36 +0800

    gonna do container with most water problem tomorrow

---
+++ arrays_hashmaps/containerwithmostwater.py
@@ -0,0 +1,8 @@
+""" You are given an integer array heights where heights[i] represents the height of the 
+ith bar.You may choose any two bars to form a container. 
+Return the maximum amount of water a container can store. """
+
+
+class Solution():
+    def maxArea(self, heights):
+        
